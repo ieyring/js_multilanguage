03462983cf7ba2d3b65d210f62ebcbfbb635d771
ieyring <ieyring@users.noreply.github.com>
2016-07-01 12:13:30 +0200

    Create locales.js

---
+++ locales.js
@@ -0,0 +1,12 @@
+var locales = { 
+   en: { 
+     hello: 'Hello',
+     about: 'About',
+     messages: 'Messages'
+   }, 
+   de: { 
+     hello: 'Hallo',
+     about: 'Über',
+     messages: 'Nachrichten'
+   } 
+};
